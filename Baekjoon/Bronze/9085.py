bedbde3628174edfca958b2817f3df72b5c52cd3
doge0n <dlehrjs3191@g.kmou.ac.kr>
2025-12-28 22:16:41 +0900

    백준 9085번

---
+++ Baekjoon/Bronze/9085.py
@@ -0,0 +1,17 @@
+#백준 9085번
+#브론즈3번
+#2025-12-28
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+
+for _ in range(T):
+    N = int(input())
+
+    sum_list = [int(x) for x in input().split()]
+    print(sum(sum_list))
+
+    
