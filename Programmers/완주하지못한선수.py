258296e5291c07662f3b29b6fa61a25a5ca19500
doge0n <dlehrjs3191@g.kmou.ac.kr>
2026-01-04 15:02:48 +0900

    풀이추가

--- Programmers/완주하지못한선수.py
+++ Programmers/완주하지못한선수.py
@@ -2,6 +2,8 @@
 #해시 Level1
 #2026-01-04
 
+#풀이1
+
 def solution(participant, completion):
     hash_dict = {}
     
@@ -19,3 +21,16 @@ def solution(participant, completion):
     for name in hash_dict:
         if hash_dict[name] > 0:
             return name
+        
+
+#풀이2
+
+import collections
+
+def solution(participant, completion):
+    # 1. 각 명단의 개수를 세어주는 Counter 객체를 만든다
+    # 2. Counter끼리는 뺄셈이 가능하며, 결과로 남은 객체에서 키값을 가져온다
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    
+    # 3. 뺄셈 결과 남은 첫 번째 이름을 반환
+    return list(answer.keys())[0]
